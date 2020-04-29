a332db43b408bf4e4b7f7c9c8f074f8c7b52e8b9
choirockdam <choi37338@naver.com>
2020-04-29 13:15:15 +0900

    커스텀 다이얼로그 사용시 생기는 문제점 해결.

--- main_mango/app/src/main/java/com/example/mangoplate/src/home/search_restaurant/searchTab_layout/SearchTabLayout.java
+++ main_mango/app/src/main/java/com/example/mangoplate/src/home/search_restaurant/searchTab_layout/SearchTabLayout.java
@@ -1,21 +1,23 @@
 package com.example.mangoplate.src.home.search_restaurant.searchTab_layout;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -37,6 +39,9 @@ public class SearchTabLayout extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_tab_layout);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide(); // 이 두줄을 쓰면 타이틀 바를 없앨 수가 있습니다.
         mContext = getApplicationContext();
         mTabLayout = (TabLayout) findViewById(R.id.layout_tab);
         mTabLayout.setSelectedTabIndicatorGravity(TabLayout.INDICATOR_GRAVITY_TOP);
@@ -45,7 +50,7 @@ public class SearchTabLayout extends BaseActivity {
         mTabLayout.addTab(mTabLayout.newTab().setText("서울-강남"));
         mTabLayout.addTab(mTabLayout.newTab().setText("서울-강북"));
         mViewPager = (ViewPager) findViewById(R.id.pager_contents);
-        mViewPager.setOffscreenPageLimit(4);
+//        mViewPager.setOffscreenPageLimit(4);
         mContentPagerAdapter = new ContentsPagerAdapter(
                 getSupportFragmentManager(), mTabLayout.getTabCount());
         mViewPager.setAdapter(mContentPagerAdapter);
@@ -57,16 +62,46 @@ public class SearchTabLayout extends BaseActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 mViewPager.setCurrentItem(tab.getPosition());
             }
+
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
             }
+
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
-        setContentView(R.layout.activity_search_tab_layout);
 
+        getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setGravity(Gravity.BOTTOM);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH);
 
+//        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_OUTSIDE){
+            System.out.println("TOuch outside the dialog ******************** ");
+            finish();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // TODO Auto-generated method stub
+        Rect dialogBounds = new Rect();
+        getWindow().getDecorView().getHitRect(dialogBounds);
+        if (!dialogBounds.contains((int) ev.getX(), (int) ev.getY())) {
+            // Tapped outside so we finish the activity
+           this.finish();
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
 //        getWindow().setBackgroundDrawable( new ColorDrawable( Color.TRANSPARENT ) ) ;
